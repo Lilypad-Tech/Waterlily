850659e62f6c134ec4960963dd02b76106c42552
Luke Marsden <me@lukemarsden.net>
2023-03-10 17:48:11 +0000

    rename images to be curl-friendly

--- scripts/upload.py
+++ scripts/upload.py
@@ -9,6 +9,12 @@ def upload(path, files):
 
 def upload_files(outpath):
     prefix = os.getenv("WATERLILY_JOB_ID")
+    os.chdir(outpath)
+    os.system("mv *_0.png image_0.png")
+    os.system("mv *_1.png image_1.png")
+    os.system("mv *_2.png image_2.png")
+    os.system("mv *_3.png image_3.png")
+    os.system("mv *.jpg combined.jpg")
     upload("job/"+prefix, os.listdir(outpath))
 
 if __name__ == "__main__":
