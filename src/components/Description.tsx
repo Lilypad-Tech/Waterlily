a773c76b74275b8dec2e45848620b2158fe6300e
Alison Haire <alihaire900@gmail.com>
2023-03-15 12:50:39 +1100

    Change description

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,17 +1,11 @@
 import { FC, ReactElement } from 'react';
-import { Box, Typography } from '@mui/material';
-import { appDescription } from '@/definitions/strings';
+import { Box, Typography, Link } from '@mui/material';
+import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined';
 
-const descriptionStyle = {
-  width: '100%',
-  fontWeight: 'bold',
-  whiteSpace: 'pre-line',
-  // fontFamily: 'fantasy',
-  lineHeight: '1.7',
-};
 const boxStyle = {
   width: '100%',
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   padding: '1rem',
 };
@@ -23,9 +17,29 @@ type DescriptionProps = {
 export const Description: FC<DescriptionProps> = ({ text }): ReactElement => {
   return (
     <Box sx={boxStyle}>
-      <Typography variant="body1" sx={descriptionStyle}>
+      <Typography variant="h6">Artists Create Astonishing Value.</Typography>
+      <Typography variant="h6">They deserve to be paid.</Typography>
+      <Link
+        href="https://luck-muscle-f89.notion.site/Waterlily-2412ac513d414a8e9d60ad60d06efd8d"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingTop: '0.5rem',
+          }}
+        >
+          <Typography>See How it Works</Typography>
+          <ArrowRightAltOutlinedIcon />
+        </Box>
+      </Link>
+      {/* <Typography variant="body1" sx={descriptionStyle}>
         {text || appDescription}
-      </Typography>
+      </Typography> */}
     </Box>
   );
 };
