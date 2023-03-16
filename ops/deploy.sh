d0cb1f5a03195057f3492366fe46f53cf2607242
Kai Davenport <kaiyadavenport@gmail.com>
2023-03-16 14:34:38 +0000

    add second deployment of the bridge to point at testnet

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -18,9 +18,17 @@ gSCP ../hardhat/.env /tmp/env
 gSSH mv /tmp/env /root/lilypad.env
 gSSH chmod 0400 /root/lilypad.env
 
+gSCP ../hardhat/.env.testnet /tmp/env.testnet
+gSSH mv /tmp/env.testnet /root/lilypad.testnet.env
+gSSH chmod 0400 /root/lilypad.testnet.env
+
 gSCP lilypad.service /tmp/lilypad.service
 gSSH mv /tmp/lilypad.service /etc/systemd/system/lilypad.service
 gSSH chmod 0444 /etc/systemd/system/lilypad.service
+
+gSCP lilypad-testnet.service /tmp/lilypad-testnet.service
+gSSH mv /tmp/lilypad-testnet.service /etc/systemd/system/lilypad-testnet.service
+gSSH chmod 0444 /etc/systemd/system/lilypad-testnet.service
 
 gSCP ./../../lilypad/bin/lilypad-linux-amd64 /tmp/lilypad
 gSSH mv /tmp/lilypad /usr/bin/lilypad
@@ -28,4 +36,6 @@ gSSH chmod 0555 /usr/bin/lilypad
 
 gSSH systemctl daemon-reload
 gSSH systemctl enable lilypad
+gSSH systemctl enable lilypad-testnet
 gSSH systemctl restart lilypad
+gSSH systemctl restart lilypad-testnet
