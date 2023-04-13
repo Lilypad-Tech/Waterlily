d01ef941d9648117296acf68ce40c5d603c8a93d
Alison Haire <alihaire900@gmail.com>
2023-04-14 00:20:33 +1000

    use ImageQuickCard in generated images to take advantage of download button

--- src/components/StableDiffusion/GeneratedImages.tsx
+++ src/components/StableDiffusion/GeneratedImages.tsx
@@ -1,15 +1,23 @@
 import { useContext } from 'react';
-import { Box, Typography, CardMedia, Card } from '@mui/material';
-import { ContractContext, ImageContext, IMAGE_NUMBER_ARRAY } from '@/context';
-import { artists } from '@/definitions';
+import { Box, Typography } from '@mui/material';
+import {
+  ContractContext,
+  ArtistContext,
+  ImageContext,
+  IMAGE_NUMBER_ARRAY,
+} from '@/context';
+import { ImageQuickCard } from '@/components';
 
 export const GeneratedImages = () => {
   const { customerImages } = useContext(ContractContext);
   const { getQuickImageURL } = useContext(ImageContext);
+  const { findArtistById } = useContext(ArtistContext);
   return (
     <>
       {customerImages.map((image) => {
-        const artist = artists.find((a) => a.artistId === image.artist);
+        console.log('generated', image);
+        const artist = findArtistById(image.artist);
+        console.log('generated artist', artist);
         return (
           <Box
             key={image.id.toString()}
@@ -34,21 +42,12 @@ export const GeneratedImages = () => {
               }}
             >
               {IMAGE_NUMBER_ARRAY.map((imageNumber) => {
+                const link = getQuickImageURL(image.id.toNumber(), imageNumber);
                 return (
-                  <Card
-                    key={imageNumber}
-                    sx={{
-                      maxWidth: 250,
-                      border: '1px solid white',
-                      ml: 1,
-                      mr: 1,
-                    }}
-                  >
-                    <CardMedia
-                      component="img"
-                      image={getQuickImageURL(image.id.toNumber(), imageNumber)}
-                    />
-                  </Card>
+                  <ImageQuickCard
+                    image={{ link: link, alt: image.id, minted: false }}
+                    idx={imageNumber}
+                  />
                 );
               })}
             </Box>
