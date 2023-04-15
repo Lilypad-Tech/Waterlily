4df0e2aaba1908b58229a1fe31425196c5d88118
Alison Haire <alihaire900@gmail.com>
2023-04-16 08:44:40 +1000

    fix analytics script

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,15 +1,31 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
-declare let window: any;
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+    dataLayer: Record<string, any>;
+  }
+}
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url) => {
+export const pageview = (url: string) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
   });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+}): void => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
